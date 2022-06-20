fa9347dc7a6f7e4cee95efe8fec54b1a7ace3487
Rushanksavant <rushanksavant34@gmail.com>
2022-06-20 11:59:28 +0530

    rectified ~ flashloan spell

--- contracts/FlashLoan_Logic.sol
+++ contracts/FlashLoan_Logic.sol
@@ -20,13 +20,6 @@ contract flashLoanLogic is starters {
         string[] memory _targets = new string[](6);
         bytes[] memory _data = new bytes[](6);
 
-        _targets[0] = "INSTAPOOL-C";
-        _targets[1] = "BASIC-A";
-        _targets[2] = "COMPOUND-A";
-        _targets[3] = "COMPOUND-A";
-        _targets[4] = "UNISWAP-V2-A";
-        _targets[5] = "INSTAPOOL-C";
-
         bytes4 flashBorrow = bytes4(
             keccak256(
                 "flashBorrowAndCast(address,uint256,uint256,bytes32,bytes32)"
@@ -48,50 +41,83 @@ contract flashLoanLogic is starters {
             keccak256("flashPayback(address,uint256,uint256,uint256)")
         );
 
-        _data[0] = abi.encodeWithSelector(
-            flashBorrow,
-            0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE, // token
-            amt * 2, // amount of DAI to borrow
-            1, // route
-            abi.encode(_targets, _data), // data
-            bytes("0x") // extraData
+        // spells other than flashBorrow
+        (_targets[1], _data[1]) = (
+            "BASIC-A",
+            abi.encodeWithSelector(
+                basicDeposit,
+                0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE,
+                amt * 3,
+                0,
+                0
+            )
         );
-        _data[1] = abi.encodeWithSelector(
-            basicDeposit,
-            0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE,
-            amt * 3,
-            0,
-            0
+        (_targets[2], _data[2]) = (
+            "COMPOUND-A",
+            abi.encodeWithSelector(compoundDeposit, "ETH-A", amt * 3, 0, 0)
         );
-        _data[2] = abi.encodeWithSelector(
-            compoundDeposit,
-            "ETH-A",
-            amt * 3,
-            0,
-            0
+        (_targets[3], _data[3]) = (
+            "COMPOUND-A",
+            abi.encodeWithSelector(
+                compoundBorrow,
+                "DAI-A",
+                amtDAI, // amount of DAI to borrow
+                0,
+                0
+            )
         );
-        _data[3] = abi.encodeWithSelector(
-            compoundBorrow,
-            "DAI-A",
-            amtDAI, // amount of DAI to borrow
-            0,
-            0
+        (_targets[4], _data[4]) = (
+            "UNISWAP-V2-A",
+            abi.encodeWithSelector(
+                uniswapV2,
+                0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE,
+                0x6B175474E89094C44Da98b954EedeAC495271d0F,
+                amtDAI, // amount of DAI to swap, to get amt*2 amount of ETH
+                caculateUnitAmt(amt * 2, amtDAI, 18, 18, 1),
+                0,
+                0
+            )
         );
-        _data[4] = abi.encodeWithSelector(
-            uniswapV2,
-            0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE,
-            0x6B175474E89094C44Da98b954EedeAC495271d0F,
-            amtDAI, // amount of DAI to swap, to get amt*2 amount of ETH
-            caculateUnitAmt(amt * 2, amtDAI, 18, 18, 1),
-            0,
-            0
+        (_targets[5], _data[5]) = (
+            "INSTAPOOL-C",
+            abi.encodeWithSelector(
+                flashPayback,
+                0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE,
+                amt * 2,
+                0,
+                0
+            )
         );
-        _data[5] = abi.encodeWithSelector(
-            flashPayback,
-            0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE,
-            amt * 2,
-            0,
-            0
+
+        // flashBorrow spell
+        string[] memory spells = new string[](5);
+        bytes[] memory datas = new bytes[](5);
+
+        (spells[0], spells[1], spells[2], spells[3], spells[4]) = (
+            _targets[1],
+            _targets[2],
+            _targets[3],
+            _targets[4],
+            _targets[5]
+        );
+        (datas[0], datas[1], datas[2], datas[3], datas[4]) = (
+            _data[1],
+            _data[2],
+            _data[3],
+            _data[4],
+            _data[5]
+        );
+
+        (_targets[0], _data[0]) = (
+            "INSTAPOOL-C",
+            abi.encodeWithSelector(
+                flashBorrow,
+                0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE, // token
+                amt * 2, // amount of ETH to borrow
+                1, // route
+                abi.encode(spells, datas), // data
+                bytes("0x") // extraData
+            )
         );
 
         IDSA(myDSA).cast(_targets, _data, address(0));
